2029bb9efb13c666ef9b6637b41ca179bfb0effd
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-07 19:44:34 -0800

    Add url, reformat via black, remove python 2.

--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-        name='tictoc',
-        version='0.2',
-        description='Handy timer object.',
-        author='Matthew Clapp',
-        author_email='itsayellow+dev@gmail.com',
-        license='MIT',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3'
-            'Programming Language :: Python :: 2'
-            ],
-        keywords='timing',
-        py_modules=['tictoc'],
-        #python_requires='>=3',
-        )
+    name="tictoc",
+    version="0.2",
+    description="Handy timer object.",
+    author="Matthew Clapp",
+    author_email="itsayellow+dev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords="timing",
+    url="https://github.com/itsayellow/tictoc",
+    py_modules=["tictoc"],
+    # python_requires='>=3',
+)
