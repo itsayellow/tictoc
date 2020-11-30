c7ab6a24792f3fb59e1dc8330f633451148a7bdb
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-29 18:07:07 -0800

    Switch to PEP517 project.

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
-# setup for tictoc package
+#!/usr/bin/env python3
 
-import os.path
-from setuptools import setup
+import sys
 
-here = os.path.abspath(os.path.dirname(__file__))
+from setuptools import setup  # type: ignore
 
-setup(
-    name="tictoc",
-    version="0.2",
-    description="Handy timer object.",
-    author="Matthew Clapp",
-    author_email="itsayellow+dev@gmail.com",
-    license="MIT",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3"
-    ],
-    keywords="timing",
-    url="https://github.com/itsayellow/tictoc",
-    py_modules=["tictoc"],
-    # python_requires='>=3',
-)
+setup()
