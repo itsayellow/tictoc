e98624f2eb34313ae3892c56d289dd16d94ae45e
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-29 18:07:42 -0800

    black reformat.

--- tictoc.py
+++ tictoc.py
@@ -1,13 +1,13 @@
 import time
 import datetime
 
+
 class Timer:
     def __init__(self):
         self.start_time = time.time()
 
     def start(self):
-        """Start this timer.
-        """
+        """Start this timer."""
         self.start_time = time.time()
 
     def eltime(self):
@@ -32,16 +32,15 @@ class Timer:
 
     def progress_pr(self, frac_done=0.0, **print_args):
         el_time = time.time() - self.start_time
-        togo_time = el_time * (1/frac_done - 1)
+        togo_time = el_time * (1 / frac_done - 1)
 
         elapsed_str = str(datetime.timedelta(seconds=int(el_time)))
         togo_str = str(datetime.timedelta(seconds=int(togo_time)))
-        perc_str = "%2.f"%(frac_done*100)
+        perc_str = "%2.f" % (frac_done * 100)
 
         elapsed_str = "Elapsed: " + elapsed_str
         togo_str = "Est. Remaining: " + togo_str
         perc_str = perc_str + "% Complete"
 
         # make sure to erase end of string at least
-        print("\r%s%36s%29s"%(perc_str,togo_str,elapsed_str),
-                end="", **print_args)
+        print("\r%s%36s%29s" % (perc_str, togo_str, elapsed_str), end="", **print_args)
